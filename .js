5897593e0a1f331ec0928490c3469812fa6e7c14
PedroLuisFiguera <153235654+PedroLuisFiguera@users.noreply.github.com>
2024-01-04 22:32:33 +0100

    saludo personal y articulo de js
    html,css,js

---
+++ .js
@@ -0,0 +1,9 @@
+
+    // Función para saludar
+    function saludar() {
+      const nombre = prompt("¡Hola! ¿Cuál es tu nombre?");
+      const saludo = `¡Bienvenido, ${nombre}! Esperamos que disfrutes tu visita, este articulo es elaborado por PEDRO FIGUERA.
+     si necesitas informacion adicional,visita link al final de cada parrafo.
+     `;
+      alert(saludo);
+    }
